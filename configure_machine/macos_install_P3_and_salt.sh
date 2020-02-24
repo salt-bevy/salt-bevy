a54b9a96b658e34174e7975e0541d7d5b51ed75a
Vernon Cole on vdc_mac <vernon.cole@sling.com>
2020-02-24 15:43:12 -0700

    Salt 3000 MacOS bootstrap

--- configure_machine/macos_install_P3_and_salt.sh
+++ configure_machine/macos_install_P3_and_salt.sh
@@ -5,8 +5,7 @@ if ! command -v python3 >/dev/null 2>&1; then
 fi
 # thanks to gtmanfred for this automatic Salt installation script.
 if ! command -v salt-call >/dev/null 2>&1; then
-  export SALT_VERSION="$(curl -sL https://pypi.python.org/pypi/salt/json | python -c "import sys, json; print('.'.join(sorted([x.split('.') for x in list(json.load(sys.stdin)['releases'])])[-1]))")"
+  export SALT_VERSION="$(curl -sL https://pypi.python.org/pypi/salt/json | python -c "import sys, json; print('.'.join(sorted([x.split('.') for x in list(json.load(sys.stdin)['releases']) if x[-3:-1]!='rc'])[-1]))")"
   curl -sLo /tmp/salt.pkg "https://repo.saltstack.com/osx/salt-$SALT_VERSION-py3-x86_64.pkg"
   installer -pkg /tmp/salt.pkg -target /
 fi
-k
